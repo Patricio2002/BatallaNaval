cd01b1acf869d880a23a70c8e8c659acb4b48dbf
pato <patricio.ad26301@gmail.com>
2021-05-02 14:05:03 -0500

    Juego subido

---
+++ Juego.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Batalla Naval</title>
+</head>
+<body>
+    <form action="Juego.php" method="$_POST">
+        <label>
+            <input type="text" name="letra" value="Letra" required>
+            <input type="number" name="numero" value="numero" min= "10" max="10" required>
+        </label>
+    </form>
+</body>
+</html>
